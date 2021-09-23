a720b79c36ec696e7b7c045b769ef5f0ba5c57a3
Maik Wille <maikwille@msn.com>
2021-09-23 20:38:55 +0200

    first commit

---
+++ src/test/java/com/naki/demo/NachhilfeportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naki.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NachhilfeportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
